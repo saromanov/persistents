0c9596809b0c68cf6a850f2070e606fd67e36f02
Sergey Romanov <xxsmotur@gmail.com>
2015-06-12 23:57:03 +0500

    add missing module

---
+++ lib/persistent.ts
@@ -0,0 +1,11 @@
+module Persistent {
+   export class LinearPersistent<T> {
+       put(item:T){
+           TypeError("This is abstract method");
+        }
+
+        pop(){
+            TypeError("This is abstract method");
+            }
+    }
+}
